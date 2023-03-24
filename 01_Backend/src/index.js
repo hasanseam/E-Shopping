694e3915c7986bd24a74d7961fe919385488dfca
hasanseam <hasanseam37@gmail.com>
2023-03-24 02:10:01 +0100

    mongodb added

--- 01_Backend/src/index.js
+++ 01_Backend/src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const productRoutes = require('./routes/product');
 
 const app = express();
 
+//DB connection
+const url = 'mongodb://localhost:27017/ejtutorial';
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4 // Use IPv4, skip trying IPv6
+}
+mongoose.Promise = global.Promise;
+mongoose.connect(url, options)
+.then(() => {console.log("Connected to MongoDB")})
+.catch((err) => console.log(err));
+
 app.use(express.json());
+app.use(cookieParser());
 
 //Routers
 
